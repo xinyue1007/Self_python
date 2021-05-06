3dbb1acc7d74bf72e839f1b92e4eedb9554edf6e
xinyue1007 <851910295@qq.com>
2021-05-06 10:37:14 +0800

    Integration game settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,23 @@
 import sys
+import form as form
 import pygame
 
+from settings import Settings
+
+
 def run_game():
+
+    ##初始化pygame、设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+
+    ##screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+
     pygame.display.set_caption("Alien Invasion")
     ##set bg
-    bg_color = (230,230,230)
+    ##bg_color = (230,230,230)
     while True:
 
         for event in pygame.event.get():
@@ -16,8 +27,9 @@ def run_game():
             print(message)
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(ai_settings.bg_color)
         ##set bg
-        screen.fill(bg_color)
+        #screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
